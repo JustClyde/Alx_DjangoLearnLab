6a3a9253c83e37da208e5b4ba9181c08604bdb1a
JustClyde <hlonelamadubela@gmail.com>
2024-11-02 18:41:47 +0200

    Update admin.py

--- Introduction_to_Django/bookshelf/admin.py
+++ Introduction_to_Django/bookshelf/admin.py
@@ -1,8 +1,9 @@
+from django.db import models
 
-from django.db import models 
-class Book(models.Model): 
-    title = models.CharField(max_length=200) 
-    author = models.CharField(max_length=100) 
-    publication_year = models.IntegerField() 
-<pre><code>def __str__(self): 
-    return self.title 
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title 
