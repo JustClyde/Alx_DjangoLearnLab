06e1a5aa7d6b549241d45f20cac2033505873439
JustClyde <hlonelamadubela@gmail.com>
2024-11-10 10:36:32 +0200

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,3 +7,12 @@ urlpatterns = [
 ]
 Step 
 "views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="
+from django.urls import path
+from .views import register, user_login, user_logout
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    # Add your other URLs here
+]
