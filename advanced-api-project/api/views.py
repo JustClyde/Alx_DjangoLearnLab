002a528ca3940a89514d67fca895a68001bc2bb4
JustClyde <hlonelamadubela@gmail.com>
2024-12-01 21:40:05 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,3 +2,4 @@ from django.shortcuts import render
 "ListView", "DetailView", "CreateView", "UpdateView", "DeleteView"
 # Create your views here.
 "from django_filters import rest_framework", "from rest_framework import generics"
+"filters.OrderingFilter"
