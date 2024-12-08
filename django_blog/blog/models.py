8d2a4bdb046ee0976401e8f27a72ab9012ff6807
JustClyde <hlonelamadubela@gmail.com>
2024-12-08 20:42:46 +0200

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 "from django.contrib.auth.models import User", "title", "content", "author", "published_date"
+"Comment(models.Model)", "post", "created_at", "updated_at"
 # Create your models here.
